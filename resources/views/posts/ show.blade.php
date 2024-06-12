39224451c020fdbfed329e6b3fda0abbd8847e68
Gabriella Khayutin <khay0001@hz.nl>
2024-06-12 20:23:56 +0200

    Ensure form data is validated and saved correctly

--- resources/views/posts/ show.blade.php
+++ resources/views/posts/ show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1>{{ $post->title }}</h1>
-        <p>{{ $post->body }}</p>
+        <p>{{ $post->content }}</p>
         <a href="{{ route('posts.index') }}" class="btn btn-secondary">Back</a>
     </div>
 @endsection
