b7409aebaf14b616d67ecbe47542d04ad2d93c79
Gabriella Khayutin <khay0001@hz.nl>
2024-06-12 19:26:47 +0200

    Fix layout and add content section for posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
@@ -27,7 +26,7 @@
 
     <!-- Page Content -->
     <main>
-        {{ $slot }}
+        @yield('content')
     </main>
 </div>
 </body>
